a9fd6358c9949fc56855704c631430654b0dd600
ASHUTOSH DAS <connect.ashutoshdas@gmail.com>
2026-09-23 22:56:39 +0530

    Add data-types.js to demonstrate variable types

---
+++ data-types.js
@@ -0,0 +1,19 @@
+let name = "Ashutosh";
+let age = 19;
+let isStudent = true;
+let marks = null;
+let city;
+
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Student:", isStudent);
+console.log("Marks:", marks);
+console.log("City:", city);
+
+console.log("\nData Types:");
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isStudent);
+console.log(typeof marks);
+console.log(typeof city);
